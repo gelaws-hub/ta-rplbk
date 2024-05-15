909d38301e0f28f48b38dea1df823b5f863ee97b
aufatsaqief <168427867+aufatsaqief@users.noreply.github.com>
2024-05-15 10:28:57 +0700

    Add files via upload

---
+++ src/design_patterns/shape_factory/shapes/Square.js
@@ -0,0 +1,12 @@
+import { Shape } from '../Shape';
+import { SquareAreaStrategy } from '../../strategy/area_strategy/SquareAreaStrategy';
+import { SquarePerimeterStrategy } from '../../strategy/perimeter_strategy/SquarePerimeterStrategy';
+
+export class Square extends Shape {
+  constructor(side) {
+    super();
+    this.side = side;
+    this.setAreaStrategy(new SquareAreaStrategy());
+    this.setPerimeterStrategy(new SquarePerimeterStrategy());
+  }
+}
